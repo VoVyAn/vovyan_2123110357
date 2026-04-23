a96a6fa17041c8d97b388b1069a9e0cf31fd4c12
VoVyAn <vovyan2809@gmail.com>
2026-04-23 15:22:45 +0700

    Activate database seeder

--- vovyan_2123110357/Program.cs
+++ vovyan_2123110357/Program.cs
@@ -182,6 +182,7 @@ using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     dbContext.Database.Migrate();
+    DatabaseSeeder.Seed(dbContext);
     
     // Ensure columns exist and admin has correct role (PostgreSQL syntax)
     try {
